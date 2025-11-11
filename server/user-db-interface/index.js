c77fb41376f31756d99475865af9f54075a3f788
JACK FAN <jack.fan.dev@gmail.com>
2025-11-11 10:20:23 -0500

    rest of the changes

--- server/user-db-interface/index.js
+++ server/user-db-interface/index.js
@@ -3,13 +3,13 @@
 
 // Export all functions from specialized modules
 export { getUserFolders, addUserFolder } from './folders.js';
-export { addSavedPaper, getSavedPapers } from './papers.js';
+export { addSavedPaper, getSavedPapers, updateSavedPaper, deleteSavedPaper } from './papers.js';
 export { subscribeToFolders, subscribeToSavedPapers } from './subscriptions.js';
 export { getUserData } from './user.js';
 export { addQueryHistory, getQueryHistory, clearQueryHistory } from './queryHistory.js';
 
 // Export utility functions for external use
-export { createResponse, validateUserId, validatePaperData, validateFolderName, validateCallback } from './utils.js';
+export { createResponse, validateUserId, validatePaperData, validatePaperUpdate, validateFolderName, validateCallback } from './utils.js';
 
 // Export test function
 // export { testInterface } from './testInterface.js';
